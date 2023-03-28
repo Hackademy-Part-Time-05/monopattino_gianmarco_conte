52d153ccb45102ffefd54798062c03e7b1be0ca5
Gian Marco Conte <conte.gianmarco@yahoo.it>
2023-03-28 19:10:48 +0200

    Inizio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Selfoworks 004</title>
+</head>
+<body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav">
+                      <a class="navbar-brand" href="#">
+                        <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+                        Bootstrap
+                      </a>
+                      <a class="nav-link" href="#">Homepage</a> 
+                      <a class="nav-link" href="#">Elenco</a> 
+                      <a class="nav-link" href="#">Monopattini</a> 
+                  </div>
+              </div>
+          </div>
+      </nav>
+    </header>
+      <main>
+      </main>
+    <footer>
+    <p>Sito sviluppato da (Gian Marco Conte)</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js">
+    </script>
+</body>
+</html>
